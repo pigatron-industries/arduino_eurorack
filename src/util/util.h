3067591c32cf94b5330877f48c02f44d5a2e7e65
Robert Ellis <rob@pigatron-industries.co.uk>
2021-04-04 12:09:06 +0100

    add ifndef

--- src/util/util.h
+++ src/util/util.h
@@ -1,4 +1,8 @@
+#ifndef util_h
+#define util_h
 
 inline float smooth(float currentValue, float prevValue, float smoothing = 0.2) {
     return (currentValue*smoothing) + prevValue*(1-smoothing);
 }
+
+#endif
