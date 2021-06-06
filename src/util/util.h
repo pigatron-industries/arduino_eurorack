6800d0a8b75ad1c260f88354e6a19061a1d2e1e2
Robert Ellis <rob@pigatron-industries.co.uk>
2021-06-06 15:27:16 +0100

    cycle int function

--- src/util/util.h
+++ src/util/util.h
@@ -5,4 +5,12 @@ inline float smooth(float currentValue, float prevValue, float smoothing = 0.2) 
     return (currentValue*smoothing) + prevValue*(1-smoothing);
 }
 
+inline int cycle(int value, int max, int direction) {
+    if(direction < 0) {
+        return value == 0 ? max : value-1;
+    } else if (direction > 0) {
+        return (value+1) % max+1;
+    }
+}
+
 #endif
