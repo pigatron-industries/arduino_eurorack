284e5c3848dc27f000aea561415eb11d34405916
Robert Ellis <rob@pigatron-industries.co.uk>
2021-07-22 18:00:24 +0100

    Add TEST_COMPILE flag

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "eurorack.h"
 
-// optional includes
-#include "hardware/max11300/MAX11300Pin.h"
-#include "hardware/mcp23s17/MCP23S17InputPin.h"
-#include "hardware/mcp23s17/MCP23S17OutputPin.h"
+#ifdef TEST_COMPILE
+    #include "hardware/max11300/MAX11300Pin.h"
+    #include "hardware/mcp23s17/MCP23S17InputPin.h"
+    #include "hardware/mcp23s17/MCP23S17OutputPin.h"
+#endif
