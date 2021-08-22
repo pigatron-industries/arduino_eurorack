74847b3e13e9ddc128091ae4cd05cab9cc7e930c
Robert Ellis <rob@pigatron-industries.co.uk>
2021-08-22 13:56:10 +0100

    Made trigger input work without interrupts

--- src/hardware/TriggerInput.cpp
+++ src/hardware/TriggerInput.cpp
@@ -2,25 +2,39 @@
 
 #include <Arduino.h>
 
-uint8_t TriggerInput::inputCount = 0;
-TriggerInput* TriggerInput::inputs[10];
+uint8_t TriggerInput::interruptInputCount = 0;
+TriggerInput* TriggerInput::interruptInputs[MAX_INTERRUPT_TRIGGERS];
 
-TriggerInput::TriggerInput(uint8_t pin, bool inverted) {
+TriggerInput::TriggerInput(uint8_t pin, bool inverted, bool useInterrupt) {
     this->pin = pin;
     this->inverted = inverted;
+    this->useInterrupt = useInterrupt;
     pinMode(pin, INPUT_PULLUP);
     value = digitalRead(pin);
-    inputs[inputCount] = this;
-    inputCount++;
-    attachInterrupt(digitalPinToInterrupt(pin), interrupt, CHANGE);
+    if(useInterrupt) {
+        interruptInputs[interruptInputCount] = this;
+        interruptInputCount++;
+        attachInterrupt(digitalPinToInterrupt(pin), interrupt, CHANGE);
+    }
 }
 
 void TriggerInput::interrupt() {
-    for(uint8_t i = 0; i < inputCount; i++) {
-        TriggerInput* input = inputs[i];
+    for(uint8_t i = 0; i < interruptInputCount; i++) {
+        TriggerInput* input = interruptInputs[i];
         bool value = digitalRead(input->getPin());
         if(value != input->getValue()) {    
             input->setValue(value);
+            input->interruptFired = true;
         }
     }
+}
+
+bool TriggerInput::update() {
+    bool updatedValue = digitalRead(pin);
+    if(updatedValue != value || interruptFired) {
+        setValue(updatedValue);
+        interruptFired = false;
+        return true;
+    }
+    return false;
 }
--- src/hardware/TriggerInput.h
+++ src/hardware/TriggerInput.h
@@ -3,25 +3,30 @@
 
 #include <inttypes.h>
 
+#define MAX_INTERRUPT_TRIGGERS 10
+
 class TriggerInput {
 
 public:
-    TriggerInput(uint8_t pin, bool inverted = true);
+    TriggerInput(uint8_t pin, bool inverted = true, bool useInterrupt = true);
     uint8_t getPin() { return pin; }
     bool getValue() { return value; } 
     bool didRise() { return inverted ? didFallInternal() : didRiseInternal(); }
     bool didFall() { return inverted ? didRiseInternal() : didFallInternal(); }
+    bool update(); // need to call update if pin doesn't use interrupt
 
 private:
     uint8_t pin;
     bool value;
     bool inverted;
+    bool useInterrupt;
+    bool interruptFired;
 
     bool rose;
     bool fell;
 
-    static TriggerInput* inputs[10];
-    static uint8_t inputCount;
+    static TriggerInput* interruptInputs[MAX_INTERRUPT_TRIGGERS];
+    static uint8_t interruptInputCount;
     static void interrupt();
 
     void setValue(bool value) { 
