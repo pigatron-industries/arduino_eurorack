447ecf35bb607180019ac8125b113be3a242ec39
Robert Ellis <rob@pigatron-industries.co.uk>
2021-08-24 19:19:39 +0100

    Added seperate input and output pin classes

--- src/hardware/max11300/MAX11300AnalogOutputPin.h
+++ src/hardware/max11300/MAX11300AnalogOutputPin.h
@@ -4,16 +4,17 @@
 #include "../native/AnalogInputPin.h"
 #include "MAX11300.h"
 
-class MAX11300Pin : public AnalogInputPin {
+class MAX11300AnalogOutputPin : public AnalogInputPin {
     public:
-        MAX11300Pin(MAX11300& max11300, uint8_t pin): 
+        MAX11300AnalogOutputPin(MAX11300& max11300, uint8_t pin): 
             AnalogInputPin(pin, 12, -5, 5),
             max11300(max11300) { 
                 this->pin = pin; 
         }
 
-        int read() { return max11300.readAnalogPin(pin); }
-        float readVoltage() { return convertToVoltage(read()); }
+        void init() {
+            max11300.setPinModeAnalogOut(pin, DACNegative5to5);
+        }
 
         void write(int value) { max11300.writeAnalogPin(pin, value); }
         void writeVoltage(float voltage) { max11300.writeAnalogPin(pin, voltageScale.convertReverse(voltage)); }
