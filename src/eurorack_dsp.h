4fca775d9bf8dcf1e5dee73d97e8531c5c44d084
Robert Ellis <rob@pigatron-industries.co.uk>
2021-08-25 18:21:15 +0100

    dsp include

---
+++ src/eurorack_dsp.h
@@ -0,0 +1,6 @@
+#ifndef Eurorack_h
+#define Eurorack_h
+
+#include "dsp/Oscillator.h"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "eurorack.h"
 
 #ifdef TEST_COMPILE
+    #include "eurorack_dsp.h"
     #include "eurorack_max11300.h"
     #include "eurorack_mcp23s17.h"
 #endif
