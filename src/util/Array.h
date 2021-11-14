4a770003e2a5861ff925bb73f2f2ffb3f0a8096d
Robert Ellis <rob@pigatron-industries.co.uk>
2021-11-14 15:10:55 +0000

    const [] operator

--- src/util/Array.h
+++ src/util/Array.h
@@ -8,7 +8,8 @@ class Array {
         int _size = 0;
         T items[MAXSIZE];
 
-        T& operator [] (int i) { return items[i]; }
+        T& operator[](int i) { return items[i]; }
+        const T& operator[](int i) const { return items[i]; }
         int size() { return _size; }
         int add(const T& item) {
             items[_size] = item;
