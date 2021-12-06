4fa6fa5d5a1104797d7819bd7a6a408b8a6ca361
Robert Ellis <rob@pigatron-industries.co.uk>
2021-12-06 15:10:35 +0000

    set total delay

--- src/dsp/sample/DelayLine.cpp
+++ src/dsp/sample/DelayLine.cpp
@@ -2,11 +2,13 @@
 
 
 void DelayLine::setDelay(size_t delay) {
+    this->delay = delay;
     delayFrac = 0.0f;
-    this->delay = delay < bufferSize ? delay : bufferSize - 1;
+    this->delayInt = delay < bufferSize ? delay : bufferSize - 1;
 }
 
 void DelayLine::setDelay(float delay) {
+    this->delay = delay;
     int32_t int_delay = static_cast<int32_t>(delay);
     delayFrac = delay - static_cast<float>(int_delay);
     delayInt = static_cast<size_t>(int_delay) < bufferSize ? int_delay : bufferSize - 1;
