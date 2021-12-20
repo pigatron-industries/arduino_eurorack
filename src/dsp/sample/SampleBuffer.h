55272ebbe0a8a68d84bf86875a9d90ef5c6d0adf
Robert Ellis <rob@pigatron-industries.co.uk>
2021-12-20 15:10:51 +0000

    Added const

--- src/dsp/sample/SampleBuffer.h
+++ src/dsp/sample/SampleBuffer.h
@@ -24,8 +24,8 @@ class SampleBuffer
         float operator [] (float i) { return read(i); }
 
         bool write(const float sample);
-        const float read(int position);
-        const float read(float position);
+        const float read(int position) const;
+        const float read(float position) const;
         float calculateReadIncrement(float playbackFrequency);
 
         bool isSampleFull() { return sampleFull; }
@@ -44,7 +44,7 @@ class SampleBuffer
         float sampleFrequency;    // actual frequency of waveform
         float playbackSampleRate; // sample rate of playback
 
-        const float read(size_t position, float fraction);
+        const float read(size_t position, float fraction) const;
 };
 
 
@@ -61,18 +61,18 @@ inline bool SampleBuffer::write(const float sample) {
     return sampleFull;
 }
 
-inline const float SampleBuffer::read(int position) {
+inline const float SampleBuffer::read(int position) const {
     return buffer[position];
 }
 
-inline const float SampleBuffer::read(float position) {
+inline const float SampleBuffer::read(float position) const {
     int32_t intPosition = static_cast<int32_t>(position);
     float fracPosition = position - static_cast<float>(intPosition);
     intPosition = static_cast<size_t>(intPosition) < bufferSize ? intPosition : bufferSize - 1;
     return read(intPosition, fracPosition);
 }
 
-inline const float SampleBuffer::read(size_t intPosition, float fracPosition) {
+inline const float SampleBuffer::read(size_t intPosition, float fracPosition) const {
     float a = buffer[intPosition % bufferSize];
     float b = buffer[(intPosition + 1) % bufferSize];
     return a + (b - a) * fracPosition;
