8ec28e0c6417b1ea8bd335c963cbf77518ab67cc
Robert Ellis <rob@pigatron-industries.co.uk>
2022-01-28 21:19:14 +0000

    added waveshapes to include

--- src/eurorack_dsp.h
+++ src/eurorack_dsp.h
@@ -5,8 +5,17 @@
 #include "dsp/clock/Clock.h"
 #include "dsp/clock/ClockDivider.h"
 #include "dsp/oscillator/Oscillator.h"
+#include "dsp/oscillator/WaveOscillator.h"
 #include "dsp/filter/StateVariableFilter.h"
 #include "dsp/sample/SampleBuffer.h"
 #include "dsp/sample/DelayLine.h"
+
+#include "dsp/waveshapes/Line.h"
+#include "dsp/waveshapes/Pulse.h"
+#include "dsp/waveshapes/Saw.h"
+#include "dsp/waveshapes/Sine.h"
+#include "dsp/waveshapes/Triangle.h"
+#include "dsp/waveshapes/WaveList.h"
+#include "dsp/waveshapes/WaveSequence.h"
 
 #endif
