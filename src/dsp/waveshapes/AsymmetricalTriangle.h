ea9925e9fafe94754174ac6dc7eb801193eb8024
Robert Ellis <rob@pigatron-industries.co.uk>
2022-01-29 20:58:41 +0000

    Added AsymmetricalTriangle

---
+++ src/dsp/waveshapes/AsymmetricalTriangle.h
@@ -0,0 +1,26 @@
+#ifndef AsymmetricalTriangle_h
+#define AsymmetricalTriangle_h
+
+#include <eurorack_dsp.h>
+#include "math.h"
+
+class AsymmetricalTriangle : public WaveSequence<2, Line> {
+    public:
+        AsymmetricalTriangle();
+        void setPeakPosition(float peakPosition);
+};
+
+inline AsymmetricalTriangle::AsymmetricalTriangle() {
+    segment(0).setStartValue(-1);
+    segment(0).setEndValue(1);
+    segment(1).setStartValue(1);
+    segment(1).setEndValue(-1);
+}
+
+inline void AsymmetricalTriangle::setPeakPosition(float peakPosition) {
+    segment(0).setLength(peakPosition);
+    segment(1).setLength(1.0f-peakPosition);
+}
+
+
+#endif
