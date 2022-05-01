326ec4c4ec865661a84506ee4d431ffca389d9d4
Robert Ellis <rob@pigatron-industries.co.uk>
2022-05-01 22:51:02 +0100

    contains method

--- src/util/Array.h
+++ src/util/Array.h
@@ -15,13 +15,25 @@ class Array {
 
         T& operator[](int i) { return items[i]; }
         const T& operator[](int i) const { return items[i]; }
+        
         int size() { return _size; }
+
         int add(const T& item) {
             items[_size] = item;
             return _size++;
         }
+
         void clear() {
             _size = 0;
+        }
+
+        bool contains(T& findItem) {
+            for(int i = 0; i < _size; i++) {
+                if(items[i] == findItem) {
+                    return true;
+                }
+            }
+            return false;
         }
 
     private:
