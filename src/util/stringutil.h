c97865f595a5ae4153fcea7417d3e7cd54f46a24
Robert Ellis <rob@pigatron-industries.co.uk>
2022-06-11 17:11:24 +0100

    simple wildcard match function

--- src/util/stringutil.h
+++ src/util/stringutil.h
@@ -7,4 +7,37 @@ inline bool beginsWith(const char* str, const char* beginning) {
     return strncmp(str, beginning, strlen(beginning)) == 0;
 }
 
+inline bool wildcardMatch(const char *wild, const char *string) {
+    const char *cp = NULL, *mp = NULL;
+
+    while ((*string) && (*wild != '*')) {
+        if ((*wild != *string) && (*wild != '?')) {
+            return false;
+        }
+        wild++;
+        string++;
+    }
+
+    while (*string) {
+        if (*wild == '*') {
+            if (!*++wild) {
+                return true;
+            }
+            mp = wild;
+            cp = string+1;
+        } else if ((*wild == *string) || (*wild == '?')) {
+            wild++;
+            string++;
+        } else {
+            wild = mp;
+            string = cp++;
+        }
+    }
+
+    while (*wild == '*') {
+        wild++;
+    }
+    return !*wild;
+}
+
 #endif
