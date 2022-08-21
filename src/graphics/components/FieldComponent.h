c5b16483d61ad88ccfd0189d15f3f5bb2198a71b
Robert Ellis <rob@pigatron-industries.co.uk>
2022-08-21 17:02:44 +0100

    FieldComponent minor updates

--- src/graphics/components/FieldComponent.h
+++ src/graphics/components/FieldComponent.h
@@ -6,7 +6,7 @@
 #include "../Component.h"
 
 #ifndef EU_FOCUS_COLOUR
-    #define EU_FOCUS_COLOUR 0
+    #define EU_FOCUS_COLOUR 1
 #endif
 
 template<class G>
@@ -16,22 +16,26 @@ public:
     static const int MAX_LABEL_LENGTH = 32;
     static const int MAX_VALUE_LENGTH = 32;
 
-    FieldComponent(uint16_t width, uint16_t labelWidth, const char* label = "", uint8_t font = 1, uint16_t colourText = 0xFFFF);
+    FieldComponent(uint16_t width, uint16_t labelWidth, const char* label = "", uint8_t font = 1, uint16_t colourText = G::DEF_COLOUR_TEXT);
     virtual void layout();
     virtual void render();
 
     void setLabel(const char* text);
+    void setFormat(const char* format);
     void setValue(const char* value);
     void setValue(int value);
 
 protected:
     char label[MAX_LABEL_LENGTH];
     char value[MAX_VALUE_LENGTH];
+    char* format = "%d";
 
     uint16_t labelWidth;
     uint8_t font;
     uint16_t colourText;
-    uint16_t colourFocus = EU_FOCUS_COLOUR;
+    uint16_t colourBack = G::DEF_COLOUR_BACK;
+    uint16_t colourFocusText = G::DEF_COLOUR_FOCUS_TEXT;
+    uint16_t colourFocusBack = G::DEF_COLOUR_FOCUS_BACK;
     uint8_t paddingTop = 1;
     uint8_t paddingBottom = 1;
 
@@ -57,7 +61,7 @@ FieldComponent<G>::FieldComponent(uint16_t width, uint16_t labelWidth, const cha
 template<class G>
 void FieldComponent<G>::layout() {
     valueLeft = this->left + labelWidth;
-    valueWidth = this->width - valueLeft;
+    valueWidth = this->width - labelWidth;
     textTop = this->top + paddingTop;
     this->setHeight(this->graphicsContext->getFontHeight(font) + paddingTop + paddingBottom);
 }
@@ -76,10 +80,15 @@ void FieldComponent<G>::renderLabel() {
 }
 
 template<class G>
+void FieldComponent<G>::setFormat(const char* format) {
+    this->format = format;
+}
+
+template<class G>
 void FieldComponent<G>::renderValue() {
-    this->graphicsContext->fillRectangle(this->valueLeft, this->top, valueWidth, this->height, this->focus ? colourFocus : 0);
+    this->graphicsContext->fillRectangle(this->valueLeft, this->top, this->valueWidth, this->height, this->focus ? colourFocusBack : colourBack);
     this->graphicsContext->setFont(font);
-    this->graphicsContext->setTextColour(colourText);
+    this->graphicsContext->setTextColour(this->focus ? colourFocusText : colourText);
     this->graphicsContext->text(&value[0], this->valueLeft, textTop);
 }
 
@@ -96,7 +105,7 @@ void FieldComponent<G>::setValue(const char* value) {
 
 template<class G>
 void FieldComponent<G>::setValue(int value) {
-    snprintf_P(this->value, MAX_VALUE_LENGTH, PSTR("%d"), value);
+    snprintf(this->value, MAX_VALUE_LENGTH, format, value);
     renderValue();
 }
 
