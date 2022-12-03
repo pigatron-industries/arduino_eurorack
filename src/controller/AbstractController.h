8d85867ec9b151fe28b54d76a7e3e89555422c46
Robert Ellis <rob@pigatron-industries.co.uk>
2022-12-03 21:28:46 +0000

    call save method after changing mode

--- src/controller/AbstractController.h
+++ src/controller/AbstractController.h
@@ -6,12 +6,12 @@
 class AbstractController {
     public:
         AbstractController(int lastMode = 0) : mode(0, lastMode) {}
-        virtual void load() {};
-        virtual void save() {};
-        virtual void init() {};
-        virtual void update() {};
-        virtual int cycleMode(int amount) { mode.cycle(amount); return mode.value; }
-        virtual void cycleValue(int amount) { mode.cycle(amount); init(); }
+        virtual void load() {}
+        virtual void save() {}
+        virtual void init() {}
+        virtual void update() {}
+        virtual int cycleMode(int amount) { mode.cycle(amount); this->save(); return mode.value; }
+        virtual void cycleValue(int amount) { mode.cycle(amount); this->save(); init(); }
         virtual void setMode(int value) { mode.setValue(value); }
     
     protected: 
