4fa012644b4ea4a9118903b3fb2416ad94769ec8
Robert Ellis <rob@pigatron-industries.co.uk>
2023-06-20 18:21:42 +0100

    memmem implementation

--- src/util/stringutil.h
+++ src/util/stringutil.h
@@ -40,4 +40,36 @@ inline bool wildcardMatch(const char *wild, const char *string) {
     return !*wild;
 }
 
+
+/*
+ * The memmem() function finds the start of the first occurrence of the
+ * substring 'needle' of length 'nlen' in the memory area 'haystack' of
+ * length 'hlen'.
+ *
+ * The return value is a pointer to the beginning of the sub-string, or
+ * NULL if the substring is not found.
+ */
+inline const char* memmem(const char *haystack, size_t hlen, const char *needle, size_t nlen) {
+    int needle_first;
+    const char *p = haystack;
+    size_t plen = hlen;
+
+    if (!nlen)
+        return nullptr;
+
+    needle_first = *(unsigned char *)needle;
+
+    while (plen >= nlen && (p = memchr(p, needle_first, plen - nlen + 1)))
+    {
+        if (!memcmp(p, needle, nlen))
+            return (void *)p;
+
+        p++;
+        plen = hlen - (p - haystack);
+    }
+
+    return nullptr;
+}
+
+
 #endif
