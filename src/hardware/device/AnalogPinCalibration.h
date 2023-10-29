a3e41b4b439d741728c7bf0e377d02a560702ddc
Robert Ellis <rob@pigatron-industries.co.uk>
2023-10-29 17:16:39 +0000

    add get scale and offset methods

--- src/hardware/device/AnalogPinCalibration.h
+++ src/hardware/device/AnalogPinCalibration.h
@@ -38,6 +38,22 @@ class AnalogPinCalibration {
             return analogPin->getCalibratedScale().convert(value);
         }
 
+        int16_t getDigitalOffset() {
+            int16_t zeroPoint = analogPin->getScale().convertReverse(0);
+            int16_t calibratedZeroPoint = analogPin->getCalibratedScale().convertReverse(0);
+            return calibratedZeroPoint - zeroPoint;
+        }
+
+        int16_t getDigitalScale() {
+            int16_t positivePoint = analogPin->getScale().convertReverse(1);
+            int16_t negativePoint = analogPin->getScale().convertReverse(-1);
+            int16_t calibratedPositivePoint = analogPin->getCalibratedScale().convertReverse(1);
+            int16_t calibratedNegativePoint = analogPin->getCalibratedScale().convertReverse(-1);
+            int16_t scale = negativePoint - positivePoint;
+            int16_t calibratedScale = calibratedNegativePoint - calibratedPositivePoint;
+            return calibratedScale - scale;
+        }
+
     private:
         AbstractAnalogPin* analogPin = nullptr;
         float increment = 0;
